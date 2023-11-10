4d00d7f4defffe90b0fa8ae9f242c4f079c8bc6c
Michael <michael@ayarlabs.com>
2023-11-10 12:42:44 -0500

    tkinter example

--- source/drivers/keithley2400.py
+++ source/drivers/keithley2400.py
@@ -10,6 +10,16 @@ class keithley2400:
     def _id(self):
         id = self.instr.query('*IDN?')
         return id
+    
+    @property
+    def voltage(self):
+        reply = self.instr.query('*IDN?')
+        return reply
+    
+    @voltage.setter
+    def voltage(self, value: float):
+        self._write(f'DISPLAY:WINDOW:TRACE:Y:RLEVEL {value} dBm')
+
 
     def vi_sweep(start, stop, step):
         voltages, currents = [], []
