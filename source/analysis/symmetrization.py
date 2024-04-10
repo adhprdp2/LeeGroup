985b6b98bbab12c0c216cb733b5961a19b5854c9
Michael Rust <mrust@Melville.local>
2024-04-10 12:37:59 -0400

    temp cluster algorithm

--- source/analysis/symmetrization.py
+++ source/analysis/symmetrization.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from pathlib import Path
 from datetime import datetime
@@ -23,12 +24,26 @@ def parse_file(filepath):
 
     return metadata, data_df
 
+def symmetrize(data_df):
+    # groupby temperature, then symmetrize
+    threshold = 0.050 # can resolve cluster with temperature variation up to 20 mK
+    data_df['temperature_cluster'] = data_df.sort_values(by=['Temperature (K)'])['Temperature (K)'].diff().gt(threshold).cumsum().add(1)
+
+    return data_df
+
 
 def main():
     metadata, data_df = parse_file(DATA_PATH / FILE_NAME)
 
+    # datetime formatting from epoch floats
     formatted_time = metadata['file_open_time'].strftime('%Y-%m-%d %H:%M:%S')
-    print(formatted_time)
-    print(data_df)
+    data_df['Time Stamp (sec)'] = (pd.to_datetime(data_df['Time Stamp (sec)'], unit='s'))
+
+
+    data_df = symmetrize(data_df)
+
+
+    print(data_df[['Temperature (K)', 'temperature_cluster', 'Magnetic Field (Oe)']])
+    print(data_df.columns)
 
 main()
